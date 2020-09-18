2ece4b684ab2471bcd2db9d7f01f3a311e21ab62
TheWayToJunior <developermicrosoft1932@gmail.com>
2020-09-18 19:13:15 +0300

    added comments with the task

--- Laba1/Task3.h
+++ Laba1/Task3.h
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <cstring>
 
+
+/* Задание 3
+
+Функция strcmp.
+Формат int strcmp(const char* s1, const char* s2).
+Функция сравнивает строку s1 со строкой s2 в лексикографическом порядке. Если s1<s2,
+то результат равен -1, если s1=s2, то 0, если s1>s2, то 1.
+
+*/
+
 class Task3 
 {
 public:
